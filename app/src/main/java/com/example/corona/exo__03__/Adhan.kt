6b6870029ab6d7ccb44b028827598cd6c6aac1b0
Djamaa Abdelmalek <abdelmalekdjamaa98@gmail.com>
2020-05-18 01:28:37 +0100

    api_fixed

--- app/src/main/java/com/example/corona/exo__03__/Adhan.kt
+++ app/src/main/java/com/example/corona/exo__03__/Adhan.kt
@@ -30,8 +30,10 @@ import kotlin.collections.ArrayList
 class Adhan : Service() {
     // file pour ajouter les requetes http
     lateinit var mRequestQueue: RequestQueue
-    val url = "https://muslimsalat.com/alger.json?key=1311bbd1c188a26a9b79dd6a77ae5882"
-    var mawa9it : ArrayList<String> = ArrayList()
+    val url = "http://api.aladhan.com/v1/timingsByCity?city=Algiers&country=Algeria&method=3"
+    var mawa9it : ArrayList<String> = arrayListOf(
+        "","","","",""
+    )
     override fun onBind(intent: Intent): IBinder {
         TODO("Return the communication channel to the service.")
     }
@@ -48,25 +50,26 @@ class Adhan : Service() {
 
 
         // Création d'une requte http de type Get
-        val mJsonObjectRequest =StringRequest(Request.Method.GET, "https://muslimsalat.com/alger.json?key=1311bbd1c188a26a9b79dd6a77ae5882",
+        val mJsonObjectRequest =StringRequest(Request.Method.GET, url,
             Response.Listener { response ->
 
                 try
                 {
                     var obj = JSONObject(response)
-                    var jsonArray: JSONArray = obj.getJSONArray("items")
+                    var data: JSONObject = obj.getJSONObject("data")
                     var gson = Gson()
-                    var jsonObject = jsonArray.getJSONObject(0)
+                    var jsonObject = data.getJSONObject("timings")
                     var day = gson.fromJson(jsonObject.toString(), Day::class.java)
                     mawa9it = arrayListOf(
-                        day.fajr!!.toUpperCase(),day.dhuhr!!.toUpperCase(),
-                        day.asr!!.toUpperCase(),day.maghrib!!.toUpperCase(),
-                        day.isha!!.toUpperCase()
+                        day.fajr!!,day.dhuhr!!,
+                        day.asr!!,day.maghrib!!,
+                        day.isha!!
                     )
                     Log.d("response",mawa9it.toString())
 
                 }
                 catch (e:Exception){
+
                 }
                 //it : JSON Object
             }, Response.ErrorListener {
@@ -76,15 +79,6 @@ class Adhan : Service() {
         // Ajouter la requete http construit dans la file des requetes http
         // pour le lancer
         mRequestQueue.add(mJsonObjectRequest)
-
-        // si la base de données est hors service
-        if (mawa9it.isEmpty()){
-            mawa9it = arrayListOf(
-                "4:01 AM","12:45 AM",
-                "4.30 PM","7:53 PM",
-                "9:15 PM"
-            )
-        }
     }
 
 
@@ -97,17 +91,15 @@ class Adhan : Service() {
         var a : String
         someHandler.postDelayed(object : Runnable {
             override fun run() {
-                hour = SimpleDateFormat("hh:mm a", Locale.US).format(Date())
-                if(hour[0].toString()=="0"){
-                    hour = hour.substring(1)
-                }
-                if(hour == "1:00 AM"){
+                hour = SimpleDateFormat("HH:mm", Locale.US).format(Date())
+                if(hour == "01:00"){
                     getData()
+                    Log.d("update","day update")
                 }
-
                 for(i in 0 until mawa9it.size){
                     if(hour == mawa9it.get(i))
                     {
+
                         val salat = getResources().getStringArray(R.array.salawat)[i]
                         val notification = NotificationCompat.Builder(baseContext,"FCM_CHANNEL_ID")
                             .setSmallIcon(R.drawable.ic_architecture_and_city)
@@ -126,7 +118,7 @@ class Adhan : Service() {
                 someHandler.postDelayed(this, 60000)
             }
         }, 10)
-        return super.onStartCommand(intent, flags, startId)
+        return START_REDELIVER_INTENT
 
     }
 
--- app/src/main/java/com/example/corona/exo__03__/Class/Day.kt
+++ app/src/main/java/com/example/corona/exo__03__/Class/Day.kt
@@ -1,11 +1,33 @@
 package com.example.corona.exo__03__.Class
 
+import com.google.gson.annotations.SerializedName
+
 data class Day(
+
+	@field:SerializedName("Sunset")
+	val sunset: String? = null,
+
+	@field:SerializedName("Asr")
 	val asr: String? = null,
+
+	@field:SerializedName("Isha")
 	val isha: String? = null,
-	val shurooq: String? = null,
-	val dateFor: String? = null,
-	val dhuhr: String? = null,
+
+	@field:SerializedName("Fajr")
 	val fajr: String? = null,
-	val maghrib: String? = null
+
+	@field:SerializedName("Dhuhr")
+	val dhuhr: String? = null,
+
+	@field:SerializedName("Maghrib")
+	val maghrib: String? = null,
+
+	@field:SerializedName("Sunrise")
+	val sunrise: String? = null,
+
+	@field:SerializedName("Midnight")
+	val midnight: String? = null,
+
+	@field:SerializedName("Imsak")
+	val imsak: String? = null
 )
